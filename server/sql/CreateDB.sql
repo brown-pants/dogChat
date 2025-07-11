2b95d863069b89fa4cc08679f7f86a8ce19a59f2
brown-pants <z19986938930@163.com>
2025-07-11 09:53:20 +0800

    update db

--- server/sql/CreateDB.sql
+++ server/sql/CreateDB.sql
@@ -9,6 +9,15 @@ CREATE TABLE users
     `profile_url` VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE friends
+(
+    `userA`     VARCHAR(255) NOT NULL,
+    `userB`     VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`userA`, `userB`),
+    FOREIGN KEY (`userA`) REFERENCES `users` (`name`),
+    FOREIGN KEY (`userB`) REFERENCES `users` (`name`)
+);
+
 DELIMITER $$
 
 CREATE PROCEDURE `reg_user`(
@@ -34,6 +43,37 @@ BEGIN
     ELSE
         -- 在users表中插入新记录
         INSERT INTO `users` (`name`, `password`, `profile_url`) VALUES (new_name, new_pwd, new_profile_url);
+        SET result = 1; -- 成功
+        COMMIT;
+    END IF;
+END$$
+
+CREATE PROCEDURE `add_friend`(
+    IN `userA` VARCHAR(255),
+    IN `userB` VARCHAR(255),
+    OUT `result` INT)
+BEGIN
+    -- 如果在执行过程中遇到任何错误，则回滚事务
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        -- 回滚事务
+        ROLLBACK;
+        -- 设置返回值为-1，表示错误
+        SET result = -1;
+    END;
+    -- 开始事务
+    START TRANSACTION;
+    -- 检查用户名是否存在
+    IF NOT EXISTS (SELECT 1 FROM `users` WHERE `name` = userA) OR NOT EXISTS (SELECT 1 FROM `users` WHERE `name` = userB) THEN
+        SET result = 2;
+        COMMIT;
+    -- 检查好友是否存在
+    ELSEIF EXISTS (SELECT 1 FROM `friends` WHERE `userA` = userA AND "userB" = userB) THEN
+        SET result = 3;
+        COMMIT;
+    ELSE
+        -- 在friends表中插入新记录
+        INSERT INTO `friends` (`userA`, `userB`) VALUES (userA, userB);
         SET result = 1; -- 成功
         COMMIT;
     END IF;
